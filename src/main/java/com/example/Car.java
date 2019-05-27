0855344df3e1bf1395c62836b28c73b66a8dd06c
risa.nazato <risa.nazato@rakus.co.jp>
2019-05-27 13:46:01 +0900

    gitへのプッシュ方法復習

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Car {
+	private Integer gas;
+	private String name;
+	
+	public Integer getGas() {
+		return gas;
+	}
+	public void setGas(Integer gas) {
+		this.gas = gas;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
---
+++ src/main/java/com/example/GitSample2Nazato1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSample2Nazato1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSample2Nazato1Application.class, args);
+	}
+
+}
